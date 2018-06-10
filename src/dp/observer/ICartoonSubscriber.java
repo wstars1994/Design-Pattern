351ea2766eefc0f95a0b1f5fd14965adfe714693
wstars <545640807@qq.com>
2018-06-10 09:10:48 +0800

    Bridge

--- src/dp/observer/ICartoonSubscriber.java
+++ src/dp/observer/ICartoonSubscriber.java
@@ -1,9 +1,5 @@
 package dp.observer;
-/**
- * 观察者模式
- * 根据对象状态进行相应的处理
- *上午7:52:56
- */
+
 public interface ICartoonSubscriber {
 
 	public String getSubscriberName();
--- src/dp/observer/Test.java
+++ src/dp/observer/Test.java
@@ -1,8 +1,16 @@
 package dp.observer;
 
+/**
+ * Observer - 观察者模式
+ * 当观察对象的状态发生变化时通知观察者
+ * 
+ * 本例场景说明 : 在动漫网站追番,当被我订阅的动漫更新时通知我
+ *
+ */
 public class Test {
 
 	public static void main(String[] args) {
+
 		//观察者
 		ICartoonSubscriber wxk = new WXKSubscriber();
 		ICartoonSubscriber jbc = new JBCSubscriber();
